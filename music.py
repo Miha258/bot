48f769dd17f321cde47ab1474aa575ac1241f977
Miha258 <77239046+Miha258@users.noreply.github.com>
2021-08-09 12:24:49 +0300

    Update music.py

--- music.py
+++ music.py
@@ -15,7 +15,17 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 
 ytdl_format_options = {
-    'format': 'bestaudio',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'audioformat' : "mp3",
+    'chachedir': False,
     'quiet': True
 }
 
